1a2667142c8e3107f105072f49a198e15ec50ab1
Alex K <alexander.kolomyts@gmail.com>
2023-08-25 10:11:12 +0300

    fix

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "<TOCKEN>"
-FORM_API_KEY = "<FORM_API_KEY>"
-YOUR_DOMAIN = "<domain>"
-PBX_QUEUES = [<number_queues>]
+TOKEN = "<TOCKEN>"  #123:AA
+FORM_API_KEY = "<FORM_API_KEY>" #ADC_qwe
+YOUR_DOMAIN = "<domain>"    #gastropartner
+PBX_QUEUES = [<queues_numbers>] #[500] або [500, 501]
